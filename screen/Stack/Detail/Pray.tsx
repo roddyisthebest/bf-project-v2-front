fd63810a60db86909d43b781a83fbfd9da1b2feb
roddyisthebest <bsy17171@naver.com>
2022-08-25 18:00:51 +0900

    [api 연동] Detail - pray

--- screen/Stack/Detail/Pray.tsx
+++ screen/Stack/Detail/Pray.tsx
@@ -1,28 +1,41 @@
-import React, {useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import {SafeAreaView, FlatList, View} from 'react-native';
 import {PrayType} from '../../../types/Pray';
 import PrayComponent from '../../../components/Pray';
+import {User} from '../../../types/User';
+import {getPraysByUserId} from '../../../api/user';
+import {initialStateProps} from '../../../store/slice';
+import {useSelector} from 'react-redux';
 const Pray = () => {
-  const [data, setData] = useState<PrayType[]>([
-    {
-      id: 1,
-      content: '로또 1등 할 수 있도록',
-      weekend: '2022-06-05',
-    },
-    {
-      id: 2,
-      content:
-        '로또 1등 할 수 있도록 로또 1등 할 수 있도록asdasd로또 1등 할 수 있도록로또 1등 할 수 있도록로또 1등 할 수 있도록',
-      weekend: '2022-06-05',
-    },
-    {
-      id: 3,
-      content:
-        '로또 1등 할 수 있도록 로또 1등 할 수 있도록로또das 1등 할 수 있도록 로또 1등 할 수 있도록로또 1등 할 수 있도록',
-      weekend: '2022-06-05',
-    },
-  ]);
+  const [data, setData] = useState<PrayType[]>([]);
+  const [disabled, setDisabled] = useState<boolean>(false);
+  const [lastId, setLastId] = useState<number>(-1);
+  const {userInfo} = useSelector((state: initialStateProps) => ({
+    userInfo: state.userInfo,
+  }));
 
+  const getData = useCallback(async (id: number, last: number) => {
+    try {
+      const {
+        data: {payload, code},
+      }: {data: {payload: PrayType[]; code: number}} = await getPraysByUserId(
+        id,
+        last,
+      );
+      if (code === 202) {
+        setDisabled(true);
+      }
+
+      setData(prev => [...prev, ...payload]);
+    } catch (e) {
+      console.log(e);
+    }
+  }, []);
+
+  useEffect(() => {
+    getData(userInfo.id, lastId);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [lastId]);
   const renderItem = ({item}: {item: PrayType}) => (
     <PrayComponent data={item} />
   );
@@ -36,6 +49,9 @@ const Pray = () => {
         )}
         renderItem={renderItem}
         keyExtractor={item => item.id.toString()}
+        onEndReached={() => {
+          setLastId(data[data.length - 1].id);
+        }}
       />
     </SafeAreaView>
   );
